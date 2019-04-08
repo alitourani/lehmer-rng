467deff4129527865353512a7b1d0adb8ef795ee
alitourani <a.tourani1991@gmail.com>
2019-04-08 21:18:34 +0430

    lehmer code in java

---
+++ src/lehmer/Lehmer.java
@@ -0,0 +1,11 @@
+/* 
+	Lehmer Calculator by Ali Tourani - 2008
+*/
+package lehmer;
+
+public class Lehmer {
+    public static void main(String[] args) {        
+        Frame1 t = new Frame1();
+        t.setVisible(true);
+    }
+}
